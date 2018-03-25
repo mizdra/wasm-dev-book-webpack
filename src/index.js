8cbb097f38b781a426aadc201e7e3d5dd1bfc0cd
mizdra <pp.mizdra@gmail.com>
2018-03-25 21:04:18 +0900

    chrore: update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+const u32 = (num) => num >>> 0
+
+export const date_now = Date.now
+
 import('./webpack_wasm_skeleton')
   .then(module => {
-    const { add } = module
+    const { add, get_timestamp, rand } = module
     console.log(add(1, 2))
+    console.log(get_timestamp())
+    console.log(u32(rand()))
   })
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,35 @@
 #![feature(proc_macro)]
 
 extern crate wasm_bindgen;
+extern crate tinymt;
+
+#[wasm_bindgen(module = "./index")]
+extern {
+    fn date_now() -> f64;
+}
 
 use wasm_bindgen::prelude::*;
+use tinymt::tinymt32;
 
-#[wasm_bindgen::prelude::wasm_bindgen]
+#[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
+}
+
+#[wasm_bindgen]
+pub fn get_timestamp() -> f64 {
+    unsafe {
+        date_now()
+    }
+}
+
+#[wasm_bindgen]
+pub fn rand() -> u32 {
+    let param = tinymt32::Param {
+        mat1: 0x8F7011EE,
+        mat2: 0xFC78FF1F,
+        tmat: 0x3793fdff,
+    };
+    let seed = 1;
+    tinymt32::from_seed(param, seed).gen()
 }
