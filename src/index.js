2e4080167d9559774b1f215ee4cbabd9d35c5775
mizdra <pp.mizdra@gmail.com>
2018-04-01 20:32:22 +0900

    refactor: rename `u32` to `toUint32`

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const u32 = (num) => num >>> 0
+const toUint32 = (num) => num >>> 0
 
 export const date_now = Date.now
 export const console_log = console.log
@@ -8,7 +8,7 @@ import('./webpack_wasm_skeleton')
     const { add, get_timestamp, rand, sum, twice, hello } = module
     console.log(add(1, 2))
     console.log(get_timestamp())
-    console.log(u32(rand()))
+    console.log(toUint32(rand()))
     console.log(sum(twice(new Int32Array([1, 2, 3, 4, 5]))))
     hello()
   })
